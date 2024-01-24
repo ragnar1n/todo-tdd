c5d34de250cdee5469a0f13e5ad1cac7bda15344
Ragnar <ragnar.naruski@voco.ee>
2024-01-25 00:13:54 +0200

    Created TodoController.getTodos unit test and integration tests

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -7,7 +7,14 @@ const createTodo = async (req, res, next) =>{
     } catch (error){
         next(error)
     }
-
+}
+const getTodos = async (req, res, next) =>{
+    try {
+        const allTodos = await TodoModel.find({});
+        res.status(200).json(allTodos);
+    } catch (error) {
+        next (error)
+    }
 }
 
-module.exports = {createTodo}
+module.exports = {createTodo, getTodos}
--- tests/integration/todo.controller.int.test.js
+++ tests/integration/todo.controller.int.test.js
@@ -14,7 +14,7 @@ describe(endpointUrl, () => {
         expect(response.body.title).toBe(newTodo.title)
         expect(response.body.done).toBe(newTodo.done)
     });
-    it('should retun error 500 on malformed data with POST' + endpointUrl, async () => {
+    it('should return error 500 on malformed data with POST' + endpointUrl, async () => {
         const response = await request(app)
             .post(endpointUrl)
             .send({title: "Missing done property"});
@@ -23,4 +23,12 @@ describe(endpointUrl, () => {
             message: "Todo validation failed: done: Path `done` is required."
         })
     });
+    test("GET " + endpointUrl, async () => {
+        const response = await request(app).get(endpointUrl);
+        expect(response.statusCode).toBe(200);
+        expect(Array.isArray(response.body)).toBeTruthy();
+        expect(response.body[0].title).toBeDefined();
+        expect(response.body[0].done).toBeDefined();
+    });
 });
+
